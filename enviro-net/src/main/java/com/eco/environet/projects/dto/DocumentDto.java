7a620663f572c089a22e5aa0aef4872322eb84db
nina <ninabu16@gmail.com>
2024-05-14 17:49:53 +0200

    ECO-35 fix: add missing fields and method

--- enviro-net/src/main/java/com/eco/environet/projects/dto/DocumentDto.java
+++ enviro-net/src/main/java/com/eco/environet/projects/dto/DocumentDto.java
@@ -1,11 +1,14 @@
 package com.eco.environet.projects.dto;
 
 import com.eco.environet.projects.model.DocumentProgress;
+import com.eco.environet.users.dto.UserDto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Data
 public class DocumentDto {
@@ -14,4 +17,6 @@ public class DocumentDto {
     private Long projectId;
     private String name;
     private DocumentProgress progress;
+    private List<TeamMemberDto> writers;
+    private List<TeamMemberDto> reviewers;
 }
